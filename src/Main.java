54814eb78cc07448881614bf9952950ddbff9d4e
VyacheslavKornilov <95097601+VyacheslavKornilov@users.noreply.github.com>
2022-01-19 19:41:37 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int bonusPrice = 20;
+        int bonusMiles = ticketPrice / bonusPrice;
+        System.out.println("Ваш бонус " + bonusMiles + " миль!");
+    }
+}
